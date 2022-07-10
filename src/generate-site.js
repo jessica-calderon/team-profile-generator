77584ccc10d7ad2bf3737cc0a7b79bb940ec723a
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-09 23:38:35 -0500

    role cards logic and html

--- src/generate-site.js
+++ src/generate-site.js
@@ -1,14 +1,78 @@
-const generateTeam (team) => {
+// generate team logic to hold cards for different roles
+const generateTeam = (team) => {
     console.log(team);
     const teamData = [];
-/*     const generateMgr = manager => {
+// generate manager card logic and html
+const generateMgr = manager => {
         console.log(manager);
         let mgrData = `
-        // todo: insert html for mgr card
+<div class="card">
+        <header class="card-header has-background-primary">
+            <p class="card-header-title has-text-white">
+            ${manager.name} <br>
+            Manager
+        </header>
+        <div class="card-content has-background-white-ter">
+            <div class="content has-background-white">
+                <ul class="list">
+                    <li>${manager.id}</li>
+                    <li><span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
+                    <li>Office Number: <a href="tel:${manager.officePhone}">${manager.officePhone}</a></li>
+                </ul>
+            </div>
+        </div>
+</div>
         `;
         teamData.push(mgrData);
-    }*/
+    }
+// generate engineer card logic and html
+const generateEngineer = engineer => {
+        console.log(engineer);
+        let engineerData = `
+<div class="card">
+        <header class="card-header has-background-primary">
+            <p class="card-header-title has-text-white">
+            ${engineer.name} <br>
+            Engineer
+        </header>
+        <div class="card-content has-background-white-ter">
+            <div class="content has-background-white">
+                <ul class="list">
+                    <li>${engineer.id}</li>
+                    <li><span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+                    <li>GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+                </ul>
+            </div>
+        </div>
+</div>
+        `;
+        teamData.push(engineerData);
+    }
 } 
+// generate intern card logic and html 
+const generateIntern = intern => {
+    console.log(intern);
+    let internData = `
+<div class="card">
+    <header class="card-header has-background-primary">
+        <p class="card-header-title has-text-white">
+        ${intern.name} <br>
+        Intern
+    </header>
+    <div class="card-content has-background-white-ter">
+        <div class="content has-background-white">
+            <ul class="list">
+                <li>${intern.id}</li>
+                <li><span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+                <li>School: ${intern.school}</li>
+            </ul>
+        </div>
+    </div>
+</div>
+    `;
+    teamData.push(internData);
+}
+
 
 // build and export html base 
 module.exports = team => {
